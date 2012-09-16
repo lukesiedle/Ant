22474ca43448726f2e9c5fd6784608213aa71fa6
Luke <luke-siedle@Luke-PackBell>
2012-09-16 12:28:18 +0200

    Updates to resource allocation.

--- 0.1.0/config/resources/clients/web.php
+++ 0.1.0/config/resources/clients/web.php
@@ -31,9 +31,8 @@
 		)
 	));
 	
-	$resources = simplexml_load_file( __DIR__ . '/includes/web.xml' );
-	
 	// Get resources from XML //
+	$resources = simplexml_load_file( __DIR__ . '/includes/web.xml' );
 	$resources = $resources->children();
 	
 	if( is_object($resources)){
@@ -51,7 +50,8 @@
 					
 					// Error checking //
 					if( !file_exists( $path )){
-						throw new Exception('Resource @ ' . $path . ' does not exist, and cannot be allocated.');
+						throw new Exception('Resource ' . $path . ' 
+							does not exist, and cannot be allocated.');
 					}
 					
 					switch( $type ){
@@ -69,7 +69,6 @@
 							Document :: addStylesheet( $path, (string) $ns );
 							break;
 					}
-					
 				}
 			}
 		}
@@ -85,71 +84,89 @@
 	// Less.php compilation of CSS files
 	// @since 0.1.0 //
 	
-	// Minify and deploy javascript (only if local)
+	// Minify and deploy javascript
 	// @return string Public filename of each compiled JavaScript file //
-	
-	if( App :: get()->local ){
-		Document :: onPrepareJavascripts( function( $javascripts, $namespace ){
-			
-			$minDir = APPLICATION_ROOT . '/public/clients/web/generated/javascript/';
-			$compiler = LIB_PATH . '/java/google-closure/compiler.jar';
-			$publicDir = 'public/clients/web/generated/javascript/';
-			$unique = '';
-		
-			
-			foreach( $javascripts as $each ){
-				$unique .= filemtime( $each );
-			}
-			
-			$filename = $namespace . '_' . substr(md5( $unique ), 5, 10) . '.js';
-			
-			$minFile = $minDir . $filename;
-			
-			if(file_exists($minFile)){
-				return $publicDir . $filename;
-			}
-			
-			$scripts = ' --js ' . implode( ' --js ', $javascripts );
-			$comm = 'java -jar ' . $compiler . ' ' . $scripts . ' --js_output_file ' . $minFile;			
-			
-			exec( $comm );
-			
-			return $publicDir . $filename;
+	Document :: onPrepareJavascripts( function( $javascripts, $namespace ){
 
-		});
-	}
+		$minDir = APPLICATION_ROOT . '/public/clients/web/generated/javascript/';
+		$compiler = LIB_PATH . '/java/google-closure/compiler.jar';
+		$publicDir = 'public/clients/web/generated/javascript/';
+		$unique = '';
+
+		foreach( $javascripts as $each ){
+			$unique .= filemtime( $each );
+		}
+
+		$filename = $namespace . '_' . substr(md5( $unique ), 5, 10) . '.js';
+
+		$minFile = $minDir . $filename;
+
+		if(file_exists($minFile)){
+			return $publicDir . $filename;
+		} else {
+			removeExisting( $minDir, $namespace );
+		}
+
+		$scripts = ' --js ' . implode( ' --js ', $javascripts );
+		$comm = 'java -jar ' . $compiler . ' ' . $scripts . ' --js_output_file ' . $minFile;			
+
+		exec( $comm );
+
+		return $publicDir . $filename;
+
+	});
 	
 	// Compile less.css stylesheets and deploy
 	// @return string Public filename of each compiled stylesheet //
-	if( App :: get()->local ){
-		
-		Document :: onPrepareStylesheets( function( $stylesheets, $namespace ) {
-			
-			$minDir = APPLICATION_ROOT . '/public/clients/web/generated/css/';
-			$publicDir = 'public/clients/web/generated/css/';
-			$unique = '';
-			$compiler = dirname(APPLICATION_ROOT) . '/lib/php/less.java.jar';
-			
-			foreach( $stylesheets as $each ){
-				$unique .= filemtime( $each );
-			}
-			
-			$filename = $namespace . '_' . substr( md5( $unique ), 5, 10 ) . '.css';
-			
-			$minFile = $minDir . $filename;
-			
-			if(file_exists($minFile)){
-				return $publicDir . $filename;
-			}
-			
-			$glue = ' --less_file ' . APPLICATION_ROOT . '/';
-			$scripts = $glue . implode( $glue , $stylesheets );
-			
-			$comm = 'java -jar ' . $compiler . ' ' . $scripts . ' --output_file ' . $minFile;			
-			
-			exec( $comm );
-			
+	Document :: onPrepareStylesheets( function( $stylesheets, $namespace ) {
+
+		$minDir = APPLICATION_ROOT . '/public/clients/web/generated/css/';
+		$publicDir = 'public/clients/web/generated/css/';
+		$unique = '';
+		$compiler = dirname(APPLICATION_ROOT) . '/lib/java/lesscss/lesscss-cli-1.3.0.jar';
+
+		foreach( $stylesheets as $each ){
+			$unique .= filemtime( $each );
+		}
+
+		$filename = $namespace . '_' . substr( md5( $unique ), 5, 10 ) . '.css';
+
+		$minFile = $minDir . $filename;
+
+		if(file_exists($minFile)){
 			return $publicDir . $filename;
-			
-		});
+		} else {
+			removeExisting( $minDir, $namespace );
+		}
+
+		$glue = ' -i ' . APPLICATION_ROOT . '/';
+		$scripts = $glue . implode( $glue , $stylesheets );
+
+		$comm = 'java -jar ' . $compiler . ' ' . $scripts . ' -o ' . $minFile;
+
+		exec( $comm );
+
+		return $publicDir . $filename;
+
+	});
+	
+	/*
+	 *	Removes existing files
+	 *	in that namespace, to 
+	 *	prevent build up
+	 *	in the cache.
+	 * 
+	 *	@since 0.1.0
+	 */
+	
+	function removeExisting( $dir, $namespace ){
+		if ($handle = opendir( $dir )) {
+			while (false !== ($entry = readdir($handle))) {
+				if( strpos($entry, $namespace) !== false ){
+					unlink( $dir . $entry );
+				}
+			}
+			closedir($handle);
+		}
 	}
+	
